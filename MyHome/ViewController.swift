f7aff7bcd952b068556ae3f82be6f0e7921036ed
Filip Sardjoski <Filip.Sardjoski@netcetera.com>
2019-07-21 21:06:44 +0200

    Remove independent action for wall and garden

--- MyHome/ViewController.swift
+++ MyHome/ViewController.swift
@@ -3,48 +3,16 @@ import CocoaMQTT
 
 class ViewController: UIViewController {
 
-    let mqttClient = CocoaMQTT(clientID: "iOS Device", host: "192.168.100.66", port: 1883)
+	let mqttClient = CocoaMQTT(clientID: "iOS Device", host: "192.168.100.66", port: 1883)
     
-    override func viewDidLoad() {
-        mqttClient.connect()
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        mqttClient.disconnect()
-    }
-    
-    @IBAction func gardenON(_ sender: Any) {
-        mqttClient.connect()
-        mqttClient.publish("/houseWallLights/esp8266", withString: "OFF")
-    }
-    
-    @IBAction func gardenOff(_ sender: Any) {
-        mqttClient.connect()
-        mqttClient.publish("/houseWallLights/esp8266", withString: "ON")
-    }
-    
-    @IBAction func gardenToggle(_ sender: Any) {
-        mqttClient.connect()
-        mqttClient.publish("/houseWallLights/esp8266", withString: "TOGGLE")
-    }
-    
-    @IBAction func houseWallOn(_ sender: Any) {
-        mqttClient.connect()
-        mqttClient.publish("/gardenLights/esp8266", withString: "OFF")
-    }
-    
-    @IBAction func houseWallOff(_ sender: Any) {
-        mqttClient.connect()
-        mqttClient.publish("/gardenLights/esp8266", withString: "ON")
-    }
-    
-    @IBAction func houseWallToggle(_ sender: Any) {
-        mqttClient.connect()
-        mqttClient.publish("/gardenLights/esp8266", withString: "TOGGLE")
-    }
-    
-    @IBAction func reconnect(_ sender: Any) {
-        mqttClient.connect()
-    }
+	@IBAction func onButtonPressed(_ sender: Any) {
+		mqttClient.publish("/houseWallLights/esp8266", withString: "ON")
+		mqttClient.publish("/gardenLights/esp8266", withString: "ON")
+	}
+	
+	@IBAction func offButtonPressed(_ sender: Any) {
+		mqttClient.publish("/houseWallLights/esp8266", withString: "OFF")
+		mqttClient.publish("/gardenLights/esp8266", withString: "ON")
+	}
 }
 
